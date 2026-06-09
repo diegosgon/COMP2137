8c1e9cfbb4f5eb85d0209d0d4eaa2527b439efab
diegosgon <sacdiego@gmail.com>
2026-06-09 11:29:22 -0400

    Add root disk usage information

--- sysreport.sh
+++ sysreport.sh
@@ -22,9 +22,13 @@ architecture=$(uname -m)
 #Gather total memory in a human-readable format.
 total_memory=$(free -h | awk '/^Mem:/ {print $2}')
 
+#Gather root filesystem disk usage.
+disk_usage=$(df -h | awk 'NR==2 {print $2 " total, " $3 " used, " $4 " available"}')
+
 echo "User Name: $current_user"
 echo "Computer Name: $computer_name"
 echo "Operating System: $os_name"
 echo "Kernel Version: $kernel_version"
 echo "Architecture: $architecture"
 echo "Total Memory: $total_memory"
+echo "Root Disk Usage: $disk_usage"
